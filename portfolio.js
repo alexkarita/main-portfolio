b8ed30912658a0bea7228092daf101c2409d9921
alexkarita <alexkarita72@gmail.com>
2024-06-21 10:43:27 +0300

    first commit

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+
+
+// /*===================scrollreveal===============*/
+// ScrollReveal({
+//      reset: true,
+//      distance: `80px`,
+//      duration: 2000,
+//      delay:200
+//     });
+
+//     ScrollReveal().reveal(`.home-content, heading`, {origin: `top`});
